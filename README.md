d6fc9b58a3973eb65f7fad5734f892756cfa290b
Blaze Furmanski <blazej.furmanski@student.manchester.ac.uk>
2021-03-18 16:08:38 +0000

    Added experimental database stuff, works somehow.

--- README.md
+++ README.md
@@ -4,7 +4,12 @@
 Install node.js LTS ([LINK](https://nodejs.org/en/download/)).
 Run `npm i` in the root project directory. This will fetch all modules into node_modules/. (I haven't included them in the repo because they weight like 150MB)
 
+### Database
+(this barely works right now)
+
+You need to have a MySQL server installed. Right now the app expects the username `root` and password `test`. You'll need to run `mysql -uroot -ptest` to log into the server and then `source setup.sql`. This will create the database `chatovid` with a table called `accounts`.
+
 ## Running
 Run `node app.js` to start the server.
 
-The app can  accessed at `localhost:3000`
+The app can accessed at `localhost:3000`
--- app.js
+++ app.js
@@ -1,15 +1,37 @@
 // --- IMPORTS
 const express = require('express');
+const express_session = require('express-session');
 const path = require('path');
+const mysql = require('mysql');
+const body_parser = require('body-parser');
 
 // --- CONSTANTS
+// -- hostname
 const hostname = '127.0.0.1';
 const port = 3000;
+
+// -- mysql
+var connection = mysql.createConnection({
+	host     : 'localhost',
+        // @TODO: change the username and password
+	user     : 'root',
+	password : 'test',
+	database : 'chatovid'
+});
 
 // --- APP
 const app = express();
 
-// -- let the server use code from modules
+app.use(express_session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.use(body_parser.urlencoded({extended: true}));
+app.use(body_parser.json());
+
+// -- serve html/assets under /assets
 app.use('/assets', express.static(path.join(__dirname, 'html/assets')))
 
 // -- INDEX (MAIN PAGE)
@@ -17,9 +39,34 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, 'html/index.html'));
 });
 
-// -- LOGIN
+// -- LOGIN AND AUTH
 app.get("/login", (req, res) => {
     res.sendFile(path.join(__dirname, 'html/login.html'));
+});
+
+app.post('/auth', function(req, res) {
+    var username = req.body.username;
+    var password = req.body.password;
+
+    if (username && password) {
+        // ?TODO: Introduce error checking here, if the query fails the whole server goes down
+        connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], 
+            function(error, results, fields) {
+                if (error) throw error;
+                if (results.length > 0) {
+                    req.session.loggedin = true;
+                    req.session.username = username;
+                    res.redirect('/home');
+                } else {
+                    res.send('Incorrect Username and/or Password!');
+                }			
+
+                res.end();
+            });
+    } else {
+            res.send('Please enter Username and Password!');
+            res.end();
+    }
 });
 
 // -- SIGNUP
