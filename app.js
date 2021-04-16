badab5d307575390772e11a5574fb62c2bef1abb
Blaze Furmanski <blazej.furmanski@student.manchester.ac.uk>
2021-04-16 14:51:41 +0100

    Pushing all changes

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ const express_session = require('express-session');
 const path = require('path');
 const mysql = require('mysql');
 const body_parser = require('body-parser');
+const bcrypt = require('bcryptjs');
 
 // --- CONSTANTS
 // -- hostname
 const hostname = '127.0.0.1';
 const port = 3000;
+
+const email_pattern = new RegExp("[a-z0-9._%+-]+@student.manchester.ac.uk$");
 
 // -- mysql
 var connection = mysql.createConnection({
@@ -17,6 +20,9 @@ var connection = mysql.createConnection({
 	password : 'super_secret_password',
 	database : 'chatovid'
 });
+
+// -- bcrypt
+const bcrypt_salt_rounds = 10;
 
 // --- APP
 const app = express();
@@ -49,16 +55,24 @@ app.post('/auth', function(req, res) {
 
     if (username && password) {
         // ?TODO: Introduce error checking here, if the query fails the whole server goes down
-        connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], 
+        connection.query('SELECT * FROM accounts WHERE username = ?', [username], 
             function(error, results, fields) {
                 if (error) throw error;
                 if (results.length > 0) {
-                    req.session.loggedin = true;
-                    req.session.username = username;
-                    res.redirect('/home');
-                } else {
-                    res.send('Incorrect Username and/or Password!');
-                }			
+                    hash = results[0].password;
+                    bcrypt.compare(password, hash, function(err, result) {
+                        if (err) throw err;
+
+                        if (result) {
+                            req.session.loggedin = true;
+                            req.session.username = username;
+                            res.redirect('/home');
+
+                        } else {
+                            res.send('Incorrect Username and/or Password!');
+                        }			
+                    });
+                }
 
                 res.end();
             });
@@ -73,9 +87,67 @@ app.get("/signup", (req, res) => {
     res.sendFile(path.join(__dirname, 'html/sign-up.html'));
 });
 
+app.post('/create_account', function(req, res) {
+    var username = req.body.username.trim();
+    var password = req.body.password.trim();
+    var email = req.body.email.trim();
+
+    // make sure all entries are present
+    if (!(email && username && password)) {
+        res.send('Please fill all fields!');
+        res.end();
+        return;
+    }
+
+    // check if the email is a valid UoM email
+    if (!email_pattern.test(email)) {
+        res.send('You need to provide a valid UoM student email.');
+        res.end();
+        return;
+    }
+
+    // check if the username exists in the database
+    connection.query('SELECT * FROM accounts WHERE username = ? OR email = ?', [username, email], 
+        function(error, results, fields) {
+            if (error) throw error;
+            if (results.length > 0) {
+                res.send('This user already exists, or the email was already used!');
+                res.end();
+            } else { 
+                // add a new user to the database
+                // @TODO: add password hashing
+
+                bcrypt.genSalt(bcrypt_salt_rounds, (err, salt) => {
+                    bcrypt.hash(password, salt, (err, hash) => {
+                        if(err) throw err;
+
+                        connection.query('INSERT INTO `accounts` (`username`, `password`, `email`) VALUES (?, ?, ?);', [username, hash, email], 
+                            function(error, results, fields) {
+                                if (error) throw error;
+                                if (results.length > 0) {
+                                    req.session.loggedin = true;
+                                    req.session.username = username;
+                                }			
+
+                                res.redirect('/login');
+                                res.end();
+                            });
+                        // Set the hashed password and save the model
+                    });
+                 });
+            }
+        });
+
+});
+
 // -- FEEDBACK
 app.get("/feedback", (req, res) => {
     res.sendFile(path.join(__dirname, 'html/feedback.html'));
+});
+
+// -- HOME
+app.get("/home", (req, res) => {
+    res.sendFile(path.join(__dirname, 'html/menu.html'));
 });
 
 // -- CHAT
